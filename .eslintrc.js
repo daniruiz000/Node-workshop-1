f8601858d16600db8d39d707607b9936fd514570
Daniel Ruiz <danielruimar@gmail.com>
2023-04-22 13:28:49 +0200

    finnish exercise

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
     es2021: true,
   },
   extends: "standard",
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
     "semi": "off",
